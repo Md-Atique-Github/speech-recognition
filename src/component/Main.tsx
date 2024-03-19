1835e2bf403bf464dcd7df4296d502c8666b2b45
Md-Atique-Github <muhammadatique.bscs@gmail.com>
2024-03-19 15:01:28 +0500

    voice to text complete

--- src/component/Main.tsx
+++ src/component/Main.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import useSpeechRecognization from '../hooks/useSpeechRecognizationHook';
+import React, { useState } from "react";
+import useSpeechRecognization from "../hooks/useSpeechRecognizationHook";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMicrophone,
+  faMicrophoneSlash,
+} from "@fortawesome/free-solid-svg-icons";
 import "./Main.css";
 
 const Main = () => {
@@ -11,24 +16,89 @@ const Main = () => {
     hasRecognitionSupport,
   } = useSpeechRecognization();
 
+  const [isMicOn, setIsMicOn] = useState(false);
+  const [selectedLanguage, setSelectedLanguage] = useState("en");
+
+  const toggleMic = () => {
+    if (isListening) {
+      stopListening();
+    } else {
+      startListening(selectedLanguage);
+    }
+    setIsMicOn((prevState) => !prevState);
+  };
+
+  const handleLanguageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedLanguage(e.target.value);
+  };
+  
+
   return (
     <div className="container">
-    {hasRecognitionSupport ? (
-      <>
-        <div className="main-content"> 
-          {isListening ? <div>Your Browser is Listening currently.</div> : null}
-          {text}
-        </div>
-        <div className="button-container">
-          <button className="button" style={{ marginRight: "" }} onClick={startListening}>Start Listening</button>
-          <button className="button" onClick={stopListening}>Stop Listening</button>
-        </div>
-      </>
-    ) : (
-      <h1 className="heading">Your Browser does not support Speech Recognition.</h1>
-    )}
-  </div>
+      <div>
+        <input
+          type="radio"
+          id="english"
+          name="language"
+          value="en"
+          checked={selectedLanguage === "en"}
+          onChange={handleLanguageChange}
+        />
+        <label htmlFor="english">English</label>
+        <input
+          type="radio"
+          id="urdu"
+          name="language"
+          value="ur-PK"
+          checked={selectedLanguage === "ur-PK"}
+          onChange={handleLanguageChange}
+          className="radio2"
+        />
+        <label htmlFor="urdu">Urdu</label>
+        <input
+          type="radio"
+          id="french"
+          name="language"
+          value="fr-FR"
+          checked={selectedLanguage === "fr-FR"}
+          onChange={handleLanguageChange}
+          className="radio2"
+        />
+        <label htmlFor="french">French</label>
+        <label>
+          <input
+            type="radio"
+            value="ar-SA"
+            checked={selectedLanguage === "ar-SA"}
+            onChange={handleLanguageChange}
+            className="radio2"
+          />
+          Arabic
+        </label>
+      </div>
+      {hasRecognitionSupport ? (
+        <>
+          <div className="main-content">
+            {isListening && !text && (
+              <div>Your Browser is Listening currently ....</div>
+            )}
+            {text}
+          </div>
+          <div className="button-container">
+            <button className="button" onClick={toggleMic}>
+              <FontAwesomeIcon
+                icon={isMicOn ? faMicrophone : faMicrophoneSlash}
+              />
+            </button>
+          </div>
+        </>
+      ) : (
+        <h1 className="heading">
+          Your Browser does not support Speech Recognition.
+        </h1>
+      )}
+    </div>
   );
-}
+};
 
 export default Main;
--- src/hooks/useSpeechRecognizationHook.ts
+++ src/hooks/useSpeechRecognizationHook.ts
@@ -1,49 +1,59 @@
-import React,{useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 
-
-let recognition: any =null
-if("webkitSpeechRecognition" in window){
-    recognition = new webkitSpeechRecognition();
-    recognition.continuous = true;
-    recognition.lang = "en-US"
-} 
+let recognition: any = null;
+if ("webkitSpeechRecognition" in window) {
+  recognition = new webkitSpeechRecognition();
+  recognition.continuous = true;
+  recognition.lang = "ur-PK";
+}
 
 const useSpeechRecognization = () => {
- const [text, setText] = useState("");
- const [isListening, setIsListening] = useState(false);
+  const [text, setText] = useState("");
+  const [isListening, setIsListening] = useState(false);
 
- useEffect(()=>{
- if(!recognition)
- return
- recognition.onresult = (event:SpeechRecognitionEvent) =>{
-     console.log("onResult Event :",event);
-     setText(event.results[0][0].transcript);
-     recognition.stop();
-     setIsListening(false)
- }
- },[])
+  useEffect(() => {
+    if (!recognition) return;
 
-const startListening =()=>{
-    setText('');
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.onresult = (event: SpeechRecognitionEvent) => {
+      setText("");
+      for (let i = 0; i < event.results.length; i++) {
+        const interimTranscript = event.results[i][0].transcript;
+        setText((prevText) => prevText + interimTranscript + " ");
+      }
+    };
+
+    recognition.onend = () => {
+      if (isListening) {
+        recognition.start();
+      }
+    };
+
+    return () => {
+      if (recognition && recognition.stop) {
+        recognition.stop();
+      }
+    };
+  }, [recognition, isListening]);
+
+  const startListening = (language: string) => {
+    setText("");
     setIsListening(true);
+    recognition.lang = language;
     recognition.start();
-}
-const stopListening =()=>{
+  };
+  const stopListening = () => {
     setIsListening(false);
     recognition.stop();
-}
-return{
+  };
+  return {
     text,
     isListening,
     startListening,
     stopListening,
     hasRecognitionSupport: !!recognition,
-}
-
-}
-
-
-
-
+  };
+};
 
 export default useSpeechRecognization;
